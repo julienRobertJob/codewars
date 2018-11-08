1606593da077247869b173bbd0ca2084c41f1590
julienRobertJob <34217325+julienRobertJob@users.noreply.github.com>
2018-11-08 13:51:39 +0100

    Kata FunctionalProgramming

---
+++ kata/src/codewars/kata/FunctionalProg1.java
@@ -0,0 +1,9 @@
+package codewars.kata;
+
+import java.util.function.Function;
+
+public class FunctionalProgramming {
+  public static Function<Student, Boolean> f = 
+    s -> "John Smith".equals(s.getFullName()) 
+          && "js123".equals(s.studentNumber);
+}
